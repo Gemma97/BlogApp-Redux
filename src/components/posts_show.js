344b53c177955b49b075277f77ab919791b7931d
Gemma Massó <gmasso@visual-engin.com>
2019-07-16 11:11:16 +0200

    Add posts list and fetch_post function

---
+++ src/components/posts_show.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions/index';
+
+class PostsShow extends Component {
+    componentWillMount() {
+        this.props.fetchPost(this.props.params.id);
+    }
+
+    render() {
+        if(!this.props.post) {
+            return <div>Loading...</div>;
+        }
+
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+function mapStateToProps(state) {
+    return { post: state.posts.post };
+}
+
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
